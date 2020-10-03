67f7b9bd241941a1590b456f09be6a00e73b13e3
Imagine King <janiukbartosz@gmail.com>
2020-10-03 15:59:19 +0200

    Walking Skeleton - API

---
+++ API/Controllers/UsersController.cs
@@ -0,0 +1,36 @@
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using API.Data;
+using API.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Controllers
+{
+    public class UsersController : BaseApiController
+    {
+        private readonly DataContext _context;
+        public UsersController(DataContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<AppUser>>> GetUsers()
+        {
+            var users = await _context.Users.ToListAsync();
+
+            return users;
+            
+        }
+
+        [HttpGet("{id}")]
+        public ActionResult<AppUser> GetUser(int id)
+        {
+            var user = _context.Users.Find(id);
+
+            return user;
+            
+        }
+    }
+}
